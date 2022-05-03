ac372c3057b45ecddc6309ae239c47bb890e98a8
vattvichet <vichetvattedu@gmail.com>
2022-05-03 14:57:56 +0700

    Added more story

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -15,26 +15,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice2Button: UIButton!
     
     let stories = [
-        Story(story0: "you see a fork in the road", choice1: "Turn left", choice2: "Turn right"),
-        Story(story0: "You see a tiger", choice1: "Shout for help", choice2: "Play dead"),
-        Story(story0: "You find a treasure chest", choice1: "Open it", choice2: "Check for the trap")
+        
+        Story(title: "you see a fork in the road",
+              choice1: "Turn left",
+              choice2: "Turn right"),
+        Story(title: "You see a tiger",
+              choice1: "Shout for help",
+              choice2: "Play dead"),
+            
+        Story(title: "You find a treasure chest",
+              choice1: "Open it",
+              choice2: "Check for the trap")
     
     ]
-   
-    let story0 = "You see a fork in the road"
-    let choice1 = "Take a left"
-    let choice2 = "Take a right"
    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        storyLabel.text = story0
+        storyLabel.text = stories[0].title
+        
         choice1Button.setTitle("Take a left", for: .normal)
+        
         choice2Button.setTitle("Take a right", for: .normal)
     
     }
     
     @IBAction func choiceMade(_ sender: UIButton) {
+        
     }
     
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct Story {
-    let story0: String
+    let title: String
     let choice1: String
     let choice2: String
 }
