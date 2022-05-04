f0679aeb13820d7cab7ab3517539008c2f89809c
vattvichet <vichetvattedu@gmail.com>
2022-05-04 14:12:52 +0700

    Added stories Logic

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,37 +14,61 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyNumber = 0
+    
     let stories = [
         
         Story(title: "you see a fork in the road",
               choice1: "Turn left",
-              choice2: "Turn right"),
+              choice1Destination: 1,
+              choice2: "Turn right",
+             choice2Destination: 2),
         Story(title: "You see a tiger",
               choice1: "Shout for help",
-              choice2: "Play dead"),
+              choice1Destination: 0, // we are using 0 cus we want to be back to the default story
+              choice2: "Play dead",
+             choice2Destination: 0),
             
         Story(title: "You find a treasure chest",
               choice1: "Open it",
-              choice2: "Check for the trap")
+              choice1Destination: 0,
+              choice2: "Check for the trap",
+             choice2Destination: 0)
     
     ]
    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        storyLabel.text = stories[0].title
-        
-        choice1Button.setTitle("Take a left", for: .normal)
-        
-        choice2Button.setTitle("Take a right", for: .normal)
-    
+        updateUI()
+
     }
     
     @IBAction func choiceMade(_ sender: UIButton) {
         
+        let userChoice = sender.currentTitle
+        
+        if userChoice == stories[storyNumber].choice1{
+            
+            storyNumber = stories[storyNumber].choice1Destination
+            
+        }else {
+            
+            storyNumber = stories[storyNumber].choice2Destination
+            
+        }
+        
+        updateUI()
+        
+        }
+        
+    func updateUI(){
+        
+        storyLabel.text = stories[storyNumber].title
+        
+        choice1Button.setTitle(stories[storyNumber].choice1, for: .normal)
+        choice2Button.setTitle(stories[storyNumber].choice2, for: .normal)
     }
-    
-
 
 }
 
