216552dc8a87b80d33243f2028127eb517b277aa
vattvichet <vichetvattedu@gmail.com>
2022-05-05 15:10:02 +0700

    Finished the Destiny Project

--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -7,23 +7,38 @@ struct StoryBrain {
     
     let stories = [
         
-        Story(title: "you see a fork in the road",
-              choice1: "Turn left",
-              choice1Destination: 1,
-              choice2: "Turn right",
-              choice2Destination: 2),
+        Story(
+            title: "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: 'Need a ride, boy?'.",
+            choice1: "I'll hop in. Thanks for the help!", choice1Destination: 2,
+            choice2: "Better ask him if he's a murderer first.", choice2Destination: 1
+        ),
+        Story(
+            title: "He nods slowly, unfazed by the question.",
+            choice1: "At least he's honest. I'll climb in.", choice1Destination: 2,
+            choice2: "Wait, I know how to change a tire.", choice2Destination: 3
+        ),
+        Story(
+            title: "As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box.",
+            choice1: "I love Elton John! Hand him the cassette tape.", choice1Destination: 5,
+            choice2: "It's him or me! You take the knife and stab him.", choice2Destination: 4
+        ),
+        Story(
+            title: "What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?",
+            choice1: "The", choice1Destination: 0,
+            choice2: "End", choice2Destination: 0
+        ),
+        Story(
+            title: "As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in.",
+            choice1: "The", choice1Destination: 0,
+            choice2: "End", choice2Destination: 0 // we are using 0 cus we want to be back to the default story
+        ),
+        Story(
+            title: "You bond with the murderer while crooning verses of 'Can you feel the love tonight'. He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: 'Try the pier.'",
+            choice1: "The", choice1Destination: 0,
+            choice2: "End", choice2Destination: 0
+        )
         
-        Story(title: "You see a tiger",
-              choice1: "Shout for help",
-              choice1Destination: 0, // we are using 0 cus we want to be back to the default story
-              choice2: "Play dead",
-              choice2Destination: 0),
         
-        Story(title: "You find a treasure chest",
-              choice1: "Open it",
-              choice1Destination: 0,
-              choice2: "Check for the trap",
-              choice2Destination: 0)
     ]
     
     func getStoryText() -> String {
