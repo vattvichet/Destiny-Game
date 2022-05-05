3e38f46d5f70e02167bdfa9eb6f9d7f4b4bc1c15
vattvichet <vichetvattedu@gmail.com>
2022-05-05 15:00:28 +0700

    Finished MVC pattern

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -7,29 +7,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    var storyNumber = 0
-    
-    let stories = [
-        
-        Story(title: "you see a fork in the road",
-              choice1: "Turn left",
-              choice1Destination: 1,
-              choice2: "Turn right",
-              choice2Destination: 2),
-        Story(title: "You see a tiger",
-              choice1: "Shout for help",
-              choice1Destination: 0, // we are using 0 cus we want to be back to the default story
-              choice2: "Play dead",
-              choice2Destination: 0),
-        
-        Story(title: "You find a treasure chest",
-              choice1: "Open it",
-              choice1Destination: 0,
-              choice2: "Check for the trap",
-              choice2Destination: 0)
-        
-    ]
-    
+    var storyBrain = StoryBrain()
+       
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -38,28 +17,18 @@ class ViewController: UIViewController {
     }
     
     @IBAction func choiceMade(_ sender: UIButton) {
-        
-        let userChoice = sender.currentTitle
-        let currentStory = stories[storyNumber]
-        
-        if userChoice == currentStory.choice1 {
-            
-            storyNumber = currentStory.choice1Destination
-            
-        } else {
-            
-            storyNumber = currentStory.choice2Destination
-        }
+    
+        storyBrain.nextStory(userChoice: sender.currentTitle!) // nextStory are calling the function (nextStory)
         
         updateUI()
     }
     
-    func updateUI(){
+    func updateUI() {
         
-        storyLabel.text = stories[storyNumber].title
+        storyLabel.text = storyBrain.getStoryText()
         
-        choice1Button.setTitle(stories[storyNumber].choice1, for: .normal)
-        choice2Button.setTitle(stories[storyNumber].choice2, for: .normal)
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
     }
     
 }
--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -1,11 +1,57 @@
-//
-//  StoryBrain.swift
-//  Destini-iOS13
-//
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
 
 import Foundation
 
+struct StoryBrain {
+    
+    var storyNumber = 0
+    
+    let stories = [
+        
+        Story(title: "you see a fork in the road",
+              choice1: "Turn left",
+              choice1Destination: 1,
+              choice2: "Turn right",
+              choice2Destination: 2),
+        
+        Story(title: "You see a tiger",
+              choice1: "Shout for help",
+              choice1Destination: 0, // we are using 0 cus we want to be back to the default story
+              choice2: "Play dead",
+              choice2Destination: 0),
+        
+        Story(title: "You find a treasure chest",
+              choice1: "Open it",
+              choice1Destination: 0,
+              choice2: "Check for the trap",
+              choice2Destination: 0)
+    ]
+    
+    func getStoryText() -> String {
+        return stories[storyNumber].title
+    }
+    
+    func getChoice1() -> String {
+        return stories[storyNumber].choice1
+    }
+    
+    func getChoice2() -> String {
+        return stories[storyNumber].choice2
+    }
+    
+    
+    mutating func nextStory(userChoice: String) { //User choice ??? with the input and string
+        
+        let currentStory = stories[storyNumber]
+        
+        if userChoice == currentStory.choice1 {
+            
+            storyNumber = currentStory.choice1Destination
+            
+        } else {
+            
+            storyNumber = currentStory.choice2Destination
+        }
+    }
+    
+}
 
